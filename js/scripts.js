04dc58d0621fcb9a9e3a49dd1b608b430148f840
Higor <hmmamud@topazevolution.com>
2022-11-27 14:35:01 -0300

    Second Commit

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,4 @@ window.addEventListener('DOMContentLoaded', event => {
     const listHoursArray = document.body.querySelectorAll('.list-hours li');
     listHoursArray[new Date().getDay()].classList.add(('today'));
 })
+
